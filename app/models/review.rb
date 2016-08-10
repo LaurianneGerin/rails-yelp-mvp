32fd197f9528ccea8808072b55a8bca0e96e7f74
Laurianne Gerin <laurianne.gerin@gmail.com>
2016-08-10 15:11:27 +0200

    ajout de la reference de restaurant au tableau review

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
+  validates :content, presence: true, allow_nil: false
+  validates :rating, presence: true, allow_nil: false
+  validates :rating, length: { maximum: 5 }
 end
